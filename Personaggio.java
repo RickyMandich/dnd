b7d97c2d95a853b2560dc96dfaf2a2e244ff730d
MandichRiccardo <ricky.mandich@gmail.com>
2023-11-27 23:45:38 +0100

    aggiornamento 2023 11 27 23:45

--- Personaggio.java
+++ Personaggio.java
@@ -247,40 +247,6 @@ public class Personaggio{
         }
         return true;
     }
-
-    protected void preparazioneOrdine(Personaggio[] pg){
-        for(int i = 0; i < pg.length; i++){
-            pg[i].iniziativa = Dadi.tiro(1, 20) + pg[i].bonus[Caratteristica.destrezza].valore;
-        }
-        boolean uguale = true;
-        do {
-            uguale = false;
-            bubbleSort(pg);
-            if (pg[0].iniziativa == pg[1].iniziativa) {
-                int pareggi = 1;
-                uguale = true;
-                while(pg[pareggi].iniziativa == pg[pareggi-1].iniziativa){
-                    pareggi++;
-                }
-                for(int i = 0; i < pareggi; i++){
-                    pg[i].iniziativa = Dadi.tiro(1, 20) + pg[i].bonus[Caratteristica.destrezza].valore;
-                }
-            }
-        }while(uguale);
-    }
-
-    protected void bubbleSort(Personaggio[] pg){
-        boolean scambio;
-        do{
-            scambio = false;
-            for (int i = 1; i < pg.length; i++) {
-                if (pg[i].iniziativa > pg[i - 1].iniziativa) {
-                    programma_dnd_java.scambia(pg[i - 1], pg[i]);
-                    scambio = true;
-                }
-            }
-        }while(scambio);
-    }
     protected boolean controlloScontro(Personaggio[] pg){
         boolean scontro = true;
         int j = 0;
@@ -331,24 +297,6 @@ public class Personaggio{
     }
     protected String info(){
         return nome + "\thp:  " + hp + "\n";
-    }
-    public void combattimento(Personaggio[] pg){
-        while(controlloScontro(pg)) {
-            for(int i=0;i<pg.length;i++){
-                Interazione.output("ora tocca a " + pg[i].info());
-                if(pg[i].hp>0){
-                    Interazione.output(pg[i].elencoNemici(pg));
-                    pg[i].attacco(pg[Interazione.input("inserisci il numero relativo al personaggio, tra quelli di questo elenco, che vuoi attaccare")-1]);
-                    Interazione.output("tiro per colpire:\t" + pg[i].tiro + "\n");
-                }else if(pg[i].hp> -(pg[i].hpTot/2) && !pg[i].morto){
-                    controMorte();
-                }else{
-                    Interazione.output(pg[i].nome + " è morto, per cui passo al personaggio successivo\n");
-                    morto = true;
-                }
-            }
-        }
-        Interazione.output("lo scontro è finito");
     }
     protected void incMorte(int tiro){
         int j;
--- programma_dnd_java.java
+++ programma_dnd_java.java
@@ -6,8 +6,8 @@ public class programma_dnd_java {
         for(int i=0;i<tot;i++) {
             Interazione.output(pg[i].toString());
         }
-        pg[0].preparazioneOrdine(pg);
-        pg[0].combattimento(pg);
+        preparazioneOrdine(pg);
+        combattimento(pg);
         Interazione.close();
     }
 
@@ -15,6 +15,58 @@ public class programma_dnd_java {
         Personaggio pgTemp = pg1;
         pg1 = pg2;
         pg2 = pgTemp;
+    }
+    public static void combattimento(Personaggio[] pg){
+        while(pg[0].controlloScontro(pg)) {
+            for(int i=0;i<pg.length;i++){
+                Interazione.output("ora tocca a " + pg[i].info());
+                if(pg[i].hp>0){
+                    Interazione.output(pg[i].elencoNemici(pg));
+                    pg[i].attacco(pg[Interazione.input("inserisci il numero relativo al personaggio, tra quelli di questo elenco, che vuoi attaccare")-1]);
+                    Interazione.output("tiro per colpire:\t" + pg[i].tiro + "\n");
+                }else if(pg[i].hp> -(pg[i].hpTot/2) && !pg[i].morto){
+                    pg[i].controMorte();
+                }else{
+                    Interazione.output(pg[i].nome + " è morto, per cui passo al personaggio successivo\n");
+                    pg[i].morto = true;
+                }
+            }
+        }
+        Interazione.output("lo scontro è finito");
+    }
+
+    protected static void preparazioneOrdine(Personaggio[] pg){
+        for(int i = 0; i < pg.length; i++){
+            pg[i].iniziativa = Dadi.tiro(1, 20) + pg[i].bonus[Caratteristica.destrezza].valore;
+        }
+        boolean uguale = true;
+        do {
+            uguale = false;
+            bubbleSort(pg);
+            if (pg[0].iniziativa == pg[1].iniziativa) {
+                int pareggi = 1;
+                uguale = true;
+                while(pg[pareggi].iniziativa == pg[pareggi-1].iniziativa){
+                    pareggi++;
+                }
+                for(int i = 0; i < pareggi; i++){
+                    pg[i].iniziativa = Dadi.tiro(1, 20) + pg[i].bonus[Caratteristica.destrezza].valore;
+                }
+            }
+        }while(uguale);
+    }
+
+    protected static void bubbleSort(Personaggio[] pg){
+        boolean scambio;
+        do{
+            scambio = false;
+            for (int i = 1; i < pg.length; i++) {
+                if (pg[i].iniziativa > pg[i - 1].iniziativa) {
+                    scambia(pg[i - 1], pg[i]);
+                    scambio = true;
+                }
+            }
+        }while(scambio);
     }
 
     public static void creaPg(Personaggio[] pg, int tot){
