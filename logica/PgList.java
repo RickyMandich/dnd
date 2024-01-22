aa1ce957b4b2afead8b73af00cff49c9d5adb11f
MandichRiccardo <ricky.mandich@gmail.com>
2024-01-22 12:02:04 +0100

    aggiornamento 2024 01 22 12:01

--- logica/PgList.java
+++ logica/PgList.java
@@ -6,8 +6,8 @@ import java.util.List;
 import java.util.ListIterator;
 
 public class PgList<T extends Personaggio> implements List<T> {
-    Object[] pg;
-    int size;
+    protected Object[] pg;
+    protected int size;
 
     protected T[] addT(T[] oldArray){
         Object[] newArray = new Object[oldArray.length+1];
@@ -17,116 +17,129 @@ public class PgList<T extends Personaggio> implements List<T> {
 
     @Override
     public int size() {
-        return pg.length;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return pg[0] == null;
     }
 
     @Override
     public boolean contains(Object o) {
+        int i=0;
+        while(i<pg.length){
+            if(pg[i] == o) return true;
+            i++;
+        }
         return false;
     }
-
+/*
     @Override
     public Iterator<T> iterator() {
         return null;
     }
-
+/*/
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        Object[] newPg = new Object[pg.length];
+        int i=0;
+        for(Object o:newPg){
+            o = pg[i];
+            i++;
+        }
+        return newPg;
     }
-
+/*
     @Override
     public <T1> T1[] toArray(T1[] a) {
         return null;
     }
-
+/*
     @Override
     public boolean add(T t) {
         return false;
     }
-
+/*
     @Override
     public boolean remove(Object o) {
         return false;
     }
-
+/*
     @Override
     public boolean containsAll(Collection<?> c) {
         return false;
     }
-
+/*
     @Override
     public boolean addAll(Collection<? extends T> c) {
         return false;
     }
-
+/*
     @Override
     public boolean addAll(int index, Collection<? extends T> c) {
         return false;
     }
-
+/*
     @Override
     public boolean removeAll(Collection<?> c) {
         return false;
     }
-
+/*
     @Override
     public boolean retainAll(Collection<?> c) {
         return false;
     }
-
+/*
     @Override
     public void clear() {
 
     }
-
+/*
     @Override
     public T get(int index) {
         return null;
     }
-
+/*
     @Override
     public T set(int index, T element) {
         return null;
     }
-
+/*
     @Override
     public void add(int index, T element) {
 
     }
-
+/*
     @Override
     public T remove(int index) {
         return null;
     }
-
+/*
     @Override
     public int indexOf(Object o) {
         return 0;
     }
-
+/*
     @Override
     public int lastIndexOf(Object o) {
         return 0;
     }
-
+/*
     @Override
     public ListIterator<T> listIterator() {
         return null;
     }
-
+/*
     @Override
     public ListIterator<T> listIterator(int index) {
         return null;
     }
-
+/*
     @Override
     public List<T> subList(int fromIndex, int toIndex) {
         return null;
     }
+
+    /**/
 }
