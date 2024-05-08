33bd4bd64743b0cddda2c2896de7eddc246766db
MandichRiccardo <ricky.mandich@gmail.com>
2024-05-08 12:21:03 +0200

    aggiornamento 2024 05 08 12:20

--- logica/PgListWithArray.java
+++ logica/PgListWithArray.java
@@ -274,26 +274,26 @@ public class PgListWithArray {
         int i=0;
         do{
             if(i==pg.length) i = 0;
-            Personaggio pg = this.pg[i];
-                if(pg.indisposto()){
-                    System.out.println(pg.nome + " non può combattere");
-                }else{
-                    System.out.println(pg.nome + " ora combatterà");
-                    System.out.println("quante azioni può eseguire " + pg.nome + " in questo turno?");
-                    int azioni = Personaggio.getInt(1);
-                    while (azioni-->0){
-                        System.out.println("sto eseguendo un'azione di " + pg.nome);
-                        System.out.println("in questa azione fai un attacco?\naltrimenti suppongo che ti muovi");
-                        if(Personaggio.getBoolean()) {
-                            Personaggio[] attaccati = inputAttaccati(new Personaggio[0], !pg.amico);
-                            pg.attacca(attaccati);
-                        }else{
-                            System.out.println(pg.nome + " si sta muovendo ma questa azione non viene gestita dal programma per cui segnatela");
-                        }
+            Personaggio pg = this.pg[ i ];
+            if ( pg.indisposto ( ) ) {
+                System.out.println ( pg.nome + " non può combattere" );
+            } else {
+                System.out.println ( pg.nome + " ora combatterà" );
+                System.out.println ( "quante azioni può eseguire " + pg.nome + " in questo turno?" );
+                int azioni = Personaggio.getInt ( 1 );
+                while ( azioni-- > 0 ) {
+                    System.out.println ( "sto eseguendo un'azione di " + pg.nome );
+                    System.out.println ( "in questa azione fai un attacco?\naltrimenti suppongo che ti muovi" );
+                    if ( Personaggio.getBoolean ( ) ) {
+                        Personaggio[] attaccati = inputAttaccati ( new Personaggio[ 0 ], ! pg.amico );
+                        pg.attacca ( attaccati );
+                    } else {
+                        System.out.println ( pg.nome + " si sta muovendo ma questa azione non viene gestita dal programma per cui segnatela" );
                     }
                 }
-                i++;
-        }while (controlloMorte());
+            }
+            i++;
+        } while ( controlloMorte ( ) );
     }
 
     private Personaggio[] inputAttaccati(Personaggio[] oldAttaccati, boolean amico) {
