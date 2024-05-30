ebe42070fb827e0add2f09f355f9f2dc2bf50d71
RickyMandich <ricky.mandich@gmail.com>
2024-05-30 10:38:24 +0200

    aggiornamento 2024 05 30 12:37 rimozione di ",NULL" dai file dati

--- csv/TestVari.java
+++ csv/TestVari.java
@@ -19,13 +19,17 @@ public class TestVari {
                 String info = "";
                 System.out.println("concateno tutte le righe a eccezzione del nome dei campi");
                 for(String line: reader.tabel){
-                    if(!info.split(",")[0].equals("nome")) info = info.concat(line + "\n");
+                    if(!line.split(",")[0].equals("nome")) {
+                        info = info.concat(line + "\n");
+                        System.out.println("ho concatenato " + line);
+                    }
                 }
                 System.out.println("sostituisco\",NULL\" con \"\"");
                 info = info.replace(",NULL", "");
                 Scrittore_csv writer = new Scrittore_csv(file);
                 System.out.println("scrivo sul file i dati elaborati");
                 writer.addCsv(info);
+                writer.close();
             } catch (FileNotFoundException e) {
                 System.out.println("Il file " + file + " ha lanciato un'eccezione");
                 e.printStackTrace();
